49379760df16293080b7a4646e75d5b377c41ae9
Junhe <devopscode@126.com>
2018-07-26 16:53:39 +0800

    eureka服务 config 服务

---
+++ config-service/src/main/java/com/cloud/config/ConfigServerApplication.java
@@ -0,0 +1,14 @@
+package com.cloud.config;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * Created by Junhe on 2018/7/26
+ */
+@SpringBootApplication
+public class ConfigServerApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(ConfigServerApplication.class);
+    }
+}
---
+++ eureka-service/src/main/java/com/cloud/eureka/EurekaServiceApplication.java
@@ -0,0 +1,16 @@
+package com.cloud.eureka;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
+
+/**
+ * Created by Junhe on 2018/7/26
+ */
+@EnableEurekaServer
+@SpringBootApplication
+public class EurekaServiceApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(EurekaServiceApplication.class);
+    }
+}
