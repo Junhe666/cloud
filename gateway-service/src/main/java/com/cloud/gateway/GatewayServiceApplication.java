59b038e7f90a0f773761b954fea3da08a0493365
Junhe <devopscode@126.com>
2018-07-27 18:15:25 +0800

    gateway服务

--- gateway-service/src/main/java/com/cloud/gateway/GatewayServiceApplication.java
+++ gateway-service/src/main/java/com/cloud/gateway/GatewayServiceApplication.java
@@ -2,15 +2,21 @@ package com.cloud.gateway;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.cloud.netflix.zuul.EnableZuulServer;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
+import org.springframework.cloud.netflix.zuul.EnableZuulProxy;
+
 
 /**
  * Created by Junhe on 2018/7/26
  */
-@EnableZuulServer
+@EnableZuulProxy
+@EnableEurekaClient
+@EnableDiscoveryClient
 @SpringBootApplication
 public class GatewayServiceApplication {
     public static void main(String[] args) {
         SpringApplication.run(GatewayServiceApplication.class);
     }
+
 }
